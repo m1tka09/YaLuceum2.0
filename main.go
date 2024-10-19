ee4e5b05280b53a118c7a30d08362611e028ed78
m1tka09 <149705488+m1tka09@users.noreply.github.com>
2024-10-19 14:34:55 +0300

    Initial commit

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	// "fmt"
+	"sort"
+)
 
-func main() {
-	fmt.Println("Hello World")
+func Permutations(input string) []string {
+	var result []string
+	var backtrack func(start int)
+	runes := []rune(input)
+
+	backtrack = func(start int) {
+		if start == len(runes) {
+			result = append(result, string(runes))
+			return
+		}
+
+		seen := make(map[rune]bool)
+		for i := start; i < len(runes); i++ {
+			if seen[runes[i]] {
+				continue
+			}
+			seen[runes[i]] = true
+
+			runes[start], runes[i] = runes[i], runes[start]
+			backtrack(start + 1)
+			runes[start], runes[i] = runes[i], runes[start]
+		}
+	}
+
+	backtrack(0)
+	sort.Strings(result)
+
+	return result
 }
